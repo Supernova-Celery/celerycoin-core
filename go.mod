9b4a5c1302f934735c55096ce71f5acdd270a26b
Spoonbill <juliusp682@gmail.com>
2019-07-20 18:24:38 -0600

    Blockchain implementation Implemented a basic blockchain to build off of Changed the module to github.com/C0D2D/celerycoin-core

--- go.mod
+++ go.mod
@@ -1,1 +1,1 @@
-module "https://github.com/C0D2D/celerycoin-core"
+module github.com/C0D2D/celerycoin-core
--- main.go
+++ main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+
+	// Modules
+
+	"github.com/C0D2D/celerycoin-core/chain"
 )
 
 func main() {
-	fmt.Println("Placeholder")
+	chain := chain.InitBlockChain()
+
+	chain.AddBlock("First block after Genesis")
+	chain.AddBlock("Second block after Genesis")
+	chain.AddBlock("Third block after Genesis")
+
+	for _, block := range chain.Blocks {
+		fmt.Printf("Prev Hash: %x\n", block.PrevHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+	}
 }
